af3dff4d112dad86e52fefe47857b3c05985fcbd
Jesus Carrillo <jesuscarrillof@gmail.com>
2025-07-09 23:41:44 -0400

    git commit

--- js/scripts.js
+++ js/scripts.js
@@ -60,15 +60,48 @@ document.addEventListener('DOMContentLoaded', function(){ //Solo espera por el H
 
 console.log(5);
 
-window.onscroll = function(){
-    console.log('scroling...')
+// window.onscroll = function(){
+//     console.log('scroling...')
 
-    //Seleccionar elementos y asociarle un evento
+//     //Seleccionar elementos y asociarle un evento
 
-    const btnEnviar = document.querySelector('.boton--primario')
-    btnEnviar.addEventListener('click', function(e){ //se registra el evento dentro del parametro de la funcion
-        console.log(e);
-        e.preventDefault(); //Evita que el evento haga su accion por defecto (en este caso enviar el formulario)
-        console.log('enviando formulario')
-    })
+//     const btnEnviar = document.querySelector('.boton--primario')
+//     btnEnviar.addEventListener('click', function(e){ //se registra el evento dentro del parametro de la funcion
+//         console.log(e);
+//         e.preventDefault(); //Evita que el evento haga su accion por defecto (en este caso enviar el formulario)
+//         console.log('enviando formulario')
+//     }) 
+// }
+
+//Eventos de los input y textarea
+const datos = {
+    nombre: '',
+    email: '',   //Objeto que se ira llenando con los datos que el usuario coloque en el formulario. Las propiedades deben tener los mismos nombres de los ID de los elementos registrados en el HTML
+    mensaje: ''
+
 }
+
+
+const nombre = document.querySelector('#nombre') //Se seleccionan los elementos que se quieren validar a traves de su ID
+const email = document.querySelector('#email')
+const mensaje = document.querySelector('#mensaje')
+
+nombre.addEventListener('input', leerTexto) //Se registra el evento que se quiere recojer de ese elemento
+email.addEventListener('input', leerTexto)
+mensaje.addEventListener('input', leerTexto)
+
+function leerTexto(e){
+    //console.log(e.target.value)
+
+    datos[e.target.id] = e.target.value //Con esta sintaxis se llenan las propiedades del objeto segun corresponda mientras el usuario va rellenando el formulario
+
+    console.log(datos)
+}
+
+//El Evento submit para enviar los formularios
+
+const formulario = document.querySelector('.formulario')
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    console.log('Enviando Formulario')
+});
